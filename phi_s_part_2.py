5837b66ad871ad0a6252c4693111143a5a2f3c88
chrisrogers1234 <chrisrogers1234@yahoo.co.uk>
2015-06-24 17:23:35 +0100

    Send to suzie

--- phi_s_part_2.py
+++ phi_s_part_2.py
@@ -44,6 +44,7 @@ def get_period(data):
 
 def do_fit(graph, xmin, xmax, ymin, ymax):
     graph_fitted = ROOT.TGraph()
+    fit_parameters = {"fit_parameters":[]}
     x_list, y_list = [], []
     for i in range(graph.GetN()):
         x = ROOT.Double()
@@ -52,7 +53,34 @@ def do_fit(graph, xmin, xmax, ymin, ymax):
         if x > xmin and x < xmax and y > ymin and y < ymax:
             x_list.append(x)
             y_list.append(y)
-            graph_fitted.SetPoint(i, x, y)
+    if len(x_list) > 5:
+        fit_parameters["mean_dt"] = numpy.mean(y_list)
+        fit_parameters["std_dt"] = numpy.std(y_list)
+        fit_parameters["n_dt"] = len(y_list)
+        fit_parameters["delta_dt"] = (max(y_list)-min(y_list))/2.
+        fit_parameters["peak_dt"], peaks_canvas = peak_hist(y_list)
+    else:
+        fit_parameters["mean_dt"] = None
+        fit_parameters["std_dt"] = None
+        fit_parameters["peak_dt"] = None
+    fit_parameters["peak_dt_min"] = min([a_peak["lower_bound"] for a_peak in fit_parameters["peak_dt"]])
+    fit_parameters["peak_dt_max"] = max([a_peak["upper_bound"] for a_peak in fit_parameters["peak_dt"]])
+    n_points = 0
+    n_points_fitted = 0
+    for i in range(graph.GetN()):
+        x = ROOT.Double()
+        y = ROOT.Double()
+        graph.GetPoint(i, x, y)
+        if x > xmin and x < xmax:
+            n_points += 1
+            if fit_parameters["peak_dt_min"] < fit_parameters["peak_dt_max"]:
+                if y > fit_parameters["peak_dt_min"] and y < fit_parameters["peak_dt_max"]:
+                    n_points_fitted += 1
+                    graph_fitted.SetPoint(i, x, y)
+            else: # wrapped around...
+                if y > fit_parameters["peak_dt_min"] or y < fit_parameters["peak_dt_max"]:
+                    n_points_fitted += 1
+                    graph_fitted.SetPoint(i, x, y)
     formula = "[0]*sin(6.2831853*x/[1]+[2])+[3]"
     fit = ROOT.TF1("fit", formula)
     fit.SetParameter(0, 50)
@@ -60,44 +88,59 @@ def do_fit(graph, xmin, xmax, ymin, ymax):
     fit.SetParameter(2, -150.e3)
     fit.SetParameter(3, 350.)
     fit.SetLineColor(4)  
-    graph_fitted.Fit(fit, "", "", xmin, xmax)
+    #graph_fitted.Fit(fit, "", "", xmin, xmax)
     graph_fitted.SetMarkerStyle(6)
-    graph_fitted.SetMarkerColor(4)
+    graph_fitted.SetMarkerColor(7)
     xboa.common.root_wrapper.keep_root_object(graph_fitted)
     xboa.common.root_wrapper.keep_root_object(fit)
-    fit_parameters = {"fit_parameters":[]}
     for i in range(4):
         fit_parameters["fit_parameters"].append([fit.GetParameter(i), fit.GetParError(i)])
     fit_parameters["chi2"] = fit.GetChisquare()
     fit_parameters["n_dof"] = fit.GetNDF()
     fit_parameters["fit_function"] = formula
     fit_parameters["cut"] = {"xmin":xmin, "xmax":xmax, "ymin":ymin, "ymax":ymax}
-    if len(x_list) > 5:
-        fit_parameters["mean_dt"] = numpy.mean(y_list)
-        fit_parameters["std_dt"] = numpy.std(y_list)
-        fit_parameters["delta_dt"] = (max(y_list)-min(y_list))/2.
-        fit_parameters["peak_dt"] = peak_hist(y_list)
-    else:
-        fit_parameters["mean_dt"] = None
-        fit_parameters["std_dt"] = None
-        fit_parameters["peak_dt"] = None
+    fit_parameters["fitted_fraction"] = float(n_points_fitted)/float(n_points)
 
-    return graph_fitted, fit, fit_parameters
+    return graph_fitted, fit, peaks_canvas, fit_parameters
 
 def peak_hist(peak_list):
     canvas = common.make_root_canvas("projected peaks")
-    hist = common.make_root_histogram("projected peaks", peak_list, "#deltat [ns]", 50)
+    hist = common.make_root_histogram("projected peaks", peak_list, "#deltat [ns]", 200, xmin=0, xmax=630.)
     bins = []
     for bin_index in range(1, hist.GetNbinsX()+1):
         bins.append(hist.GetBinContent(bin_index))
     smoothing = xboa.algorithms.smoothing.GaussianSmoothing(2., 3, True)
     smoothed = smoothing.smooth(bins)
     finder = xboa.algorithms.peak_finder.WindowPeakFinder(10, -1, 1)
-    peaks = [{"peak":hist.GetBinCenter(peak+1), "magnitude":bins[peak], "err":0.} for peak in finder.find_peaks(smoothed)]
+    peak_graph_indices, peak_x, peak_y = [], [], []
+    peaks = []
+    for peak in finder.find_peaks(smoothed):
+        a_peak = {"peak":hist.GetBinCenter(peak+1), "magnitude":bins[peak], "err":0.}
+        if a_peak["magnitude"] < 0.5*max(smoothed):
+            continue
+        peak_graph_indices.append(peak)
+        for bin in range(peak, 0, -1)+range(len(bins)-1, peak, -1):
+            if smoothed[bin] < smoothed[peak]/2.:
+                peak_graph_indices.append(bin)
+                a_peak["lower_bound"] = hist.GetBinCenter(bin+1)
+                break
+        for bin in range(peak, len(bins), +1)+range(0, peak, 1):
+            if smoothed[bin] < smoothed[peak]/2.:
+                peak_graph_indices.append(bin)
+                a_peak["upper_bound"] = hist.GetBinCenter(bin+1)
+                break
+        peaks.append(a_peak)
+    for peak_graph_index in peak_graph_indices:
+        peak_x.append(hist.GetBinCenter(peak_graph_index+1))
+        peak_y.append(smoothed[peak_graph_index])
+    dummy, graph = xboa.common.make_root_graph("peaks", peak_x, "", peak_y, "")
+    graph.SetMarkerStyle(24)
+    graph.SetMarkerColor(2)
     canvas.Draw()
     hist.Draw()
+    graph.Draw('p')
     canvas.Update()
-    return peaks
+    return peaks, canvas
 
 
 def normalise_volts(volts, periodic_times):
@@ -126,6 +169,14 @@ def plot_one(plot_dir, data, fout):
     print "    Parsing data"
     time_of_peaks = sorted([x["x"]*0.2 for x in data["RF"]["pos_peak_with_errors"]])
 
+    print "  Plotting distance between bpm and rf peaks"
+    peak_time_list = data["peak_time_list"]
+    peak_delta_list = data["peak_delta_list"]
+
+    dummy, graph = common.make_root_graph("peaks", peak_time_list, "time [ns]", peak_delta_list, "dt [ns]")
+    graph.SetMarkerStyle(7)
+
+    graph_fitted, fit, peaks_canvas, fit_parameters = do_fit(graph, 150.e3, 400.e3, 0., 700.)
 
     pos_peak = numpy.mean([x["y"] for x in data["RF"]["pos_peak_with_errors"]])
     neg_peak = numpy.mean([-x["y"] for x in data["RF"]["neg_peak_with_errors"]])
@@ -133,11 +184,14 @@ def plot_one(plot_dir, data, fout):
 
     volts = [-a_volt for a_volt in data["signal"]["voltage_list"][::]]
     times = [a_time/1e-9 for a_time in data["signal"]["time_list"]]
+    time_zero = times[0]
+    times = [a_time - time_zero for a_time in times]
+
     print "    Getting period"
     period, phase, frequency_canvas = get_period(data)
 
     print "    Getting periodic times"
-    periodic_times = [relative(a_time, time_of_peaks, -period/2.) for a_time in times]
+    periodic_times = [relative(a_time, time_of_peaks, times[0]) for a_time in times]
     print "    Voltage:", peak_to_peak_voltage, "T:", period, "N_events:", len(periodic_times)
     norm_volts = normalise_volts(volts, periodic_times)
     canvas = common.make_root_canvas("times")
@@ -150,28 +204,23 @@ def plot_one(plot_dir, data, fout):
         if abs(hist.GetBinContent(bin)) < 1e-9:
             hist.SetBinContent(bin, norm_volts[i])
     hist.SetStats(False)
-    hist.SetTitle("Voltage: "+str(round(peak_to_peak_voltage, 4))+" T: "+str(round(period, 4)))
+    hist.SetTitle("Voltage: "+str(round(peak_to_peak_voltage/2., 2)))
     xboa.common.root_wrapper.keep_root_object(hist)
     canvas.Draw()
     #canvas.SetLogz()
     hist.Draw("COLZ")
-
-    print "  Plotting distance between bpm and rf peaks"
-    peak_time_list = data["peak_time_list"]
-    peak_delta_list = data["peak_delta_list"]
-
-    dummy, graph = common.make_root_graph("peaks", peak_time_list, "time [ns]", peak_delta_list, "dt [ns]")
-    graph.SetMarkerStyle(6)
     graph.Draw('p')
-
-    graph_fitted, fit, fit_parameters = do_fit(graph, 150.e3, 400.e3, 0., 700.)
-    canvas.cd()
     graph_fitted.Draw('p')
 
     canvas.Update()
+
+
+    name = plot_dir+"/V="+str(round(peak_to_peak_voltage, 2))
+    name = name.replace(".", "_")
     for format in ["png", "root"]:
-        canvas.Print(plot_dir+"/V="+str(round(peak_to_peak_voltage, 2))+"_fitted_bpm_to_rf_deltas."+format)
-        frequency_canvas.Print(plot_dir+"/V="+str(round(peak_to_peak_voltage, 2))+"_frequency_distribution."+format)
+        canvas.Print(name+"_fitted_bpm_to_rf_deltas."+format)
+        frequency_canvas.Print(name+"_frequency_distribution."+format)
+        peaks_canvas.Print(name+"_peaks."+format)
     fit_parameters["peak_to_peak_voltage"] = peak_to_peak_voltage
     fit_parameters["rf_period"] = period
     print "    Fit parameters", json.dumps(fit_parameters, indent=2)
@@ -179,7 +228,7 @@ def plot_one(plot_dir, data, fout):
     print >> fout, json.dumps(fit_parameters)
 
 def main():
-    fin = open("data_output_1.json")
+    fin = open("data_output_ref.json")
     fout = open("data_summary.json", "w")
     for i, line in enumerate(fin.readlines()):
         print "Loading next line...", i
--- phi_s_part_3.py
+++ phi_s_part_3.py
@@ -1,6 +1,8 @@
+import bisect
 import math
 import json
 
+import numpy
 import ROOT
 
 import xboa.common
@@ -13,80 +15,138 @@ def periodic(a_time, period, phase):
     a_time = a_time - base_time
     return a_time
 
-def print_voltages(data):
-    volts = [a_datum['peak_to_peak_voltage']/2. for a_datum in data]
-    for a_volt in sorted(volts):
-        print "\item", round(a_volt, 3)
+def get_parameters():
+    global MINUIT
+    optimiser = MINUIT
+    phi_s = ROOT.Double(0.) # numpy.array((1,), dtype=numpy.float64)
+    v_0 = ROOT.Double(0.) # numpy.array((1,), dtype=numpy.float64)
+    error = ROOT.Double(0.) # numpy.array((1,), dtype=numpy.float64)
+    optimiser.GetParameter(0, v_0, error)
+    optimiser.GetParameter(1, phi_s, error)
+    return v_0, phi_s
+
+def get_chi2(v_0, phi_s):
+    global DELTA_PHI_LIST, VOLTS_LIST, DELTA_PHI_ERR_LIST, VOLTS_ERR_LIST
+    phi0 = -phi_s+1e-9 #min([dphi - DELTA_PHI_ERR_LIST[i] for i, dphi in enumerate(DELTA_PHI_LIST)])
+    phi1 = math.pi/2. - phi_s #max([dphi + DELTA_PHI_ERR_LIST[i] for i, dphi in enumerate(DELTA_PHI_LIST)])
+    test_phi_list = [phi_test*(phi1-phi0)/1000.+phi0 for phi_test in range(1000, 0, -1)]
+    test_volts_list = [v_0/math.sin(phi_test+phi_s) for phi_test in test_phi_list]
+    test_list = sorted(zip(test_volts_list, test_phi_list))
+    graph = ROOT.TGraph(1000)
+    for i in range(1000):
+        graph.SetPoint(i, test_list[i][1], test_list[i][0])
+    graph.SetMarkerColor(4)
+    graph.SetMarkerStyle(6)
+    data = {"runs":[], "score":0., "phi0":phi0, "phi1":phi1, "graph":graph}
+    for i, ref_volts in enumerate(VOLTS_LIST):
+        ref_phi = DELTA_PHI_LIST[i]
+        ref_volts_err = VOLTS_ERR_LIST[i]
+        ref_phi_err = DELTA_PHI_ERR_LIST[i]
+        chi2_list = []
+        for test_volts, test_phi in test_list:
+            miss_volts = (test_volts-ref_volts)/ref_volts_err
+            miss_phi = (test_phi-ref_phi)/ref_phi_err
+            chi2_list.append(miss_volts**2 + miss_phi**2)
+        data["score"] += min(chi2_list)
+    return data
+
+def minuit_function(n_pars, pars, score, buff, err):
+    v_0, phi_s = get_parameters()
+    score[0] = get_chi2(v_0, phi_s)["score"]
+    #print v_0, phi_s, "**", score[0]
+
+def do_minuit(delta_phi_list, volts_list, delta_phi_err_list, volts_err_list, volts_min):
+    global MINUIT, DELTA_PHI_LIST, VOLTS_LIST, DELTA_PHI_ERR_LIST, VOLTS_ERR_LIST
+    optimiser = ROOT.TMinuit()
+    optimiser.SetPrintLevel(-1)
+    optimiser.DefineParameter(0, "V0", min(volts_list), 0.1, 0.01, max(volts_list))
+    optimiser.DefineParameter(1, "phi_s", -3.5, 0.1, -5., -1.)
+    optimiser.SetFCN(minuit_function)
+    MINUIT = optimiser
+    DELTA_PHI_LIST = delta_phi_list
+    VOLTS_LIST = volts_list
+    DELTA_PHI_ERR_LIST = delta_phi_err_list
+    VOLTS_ERR_LIST = volts_err_list
+    args = numpy.array([500, 1e-6], dtype=numpy.float64)
+    ierr = ROOT.Long()
+    optimiser.mnexcm("SIMPLEX", args, 2, ierr)
+    v_0, phi_s = get_parameters()
+    return v_0, phi_s
 
 def do_fit(volts_list, delta_phi_list, delta_phi_err_list, color, volts_min, volts_max):
     graph_fit = ROOT.TGraphErrors()
     graph_plot = ROOT.TGraphErrors()
+    volts_min_index = -1
     for i, volts in enumerate(volts_list):
         graph_plot.SetPoint(i, delta_phi_list[i], volts)
         graph_plot.SetPointError(i, delta_phi_err_list[i], 0.005)
         if volts < volts_min or volts > volts_max:
+            volts_min_index = i
             continue
         graph_fit.SetPoint(i, delta_phi_list[i], volts)
         graph_fit.SetPointError(i, delta_phi_err_list[i], 0.005)
-        print "    V:", volts, "fit dphi:", delta_phi_list[i], "err(dphi):", delta_phi_err_list[i]
+        #print "    V:", volts, "dphi:", delta_phi_list[i], "err(dphi):", delta_phi_err_list[i]
     graph_plot.SetMarkerColor(color)
     graph_plot.SetMarkerStyle(25)
     graph_plot.Draw('p')
     graph_fit.Draw('p')
-    fit = ROOT.TF1("fit", "[0]/sin(x+[1])")
-    fit.SetLineColor(color)
-    fit.SetParLimits(0, 0.835302652144/2., 1.02319362021/2.)
-    fit.SetParameter(1, 3.0)
-    graph_fit.Fit(fit, "EX0")
-    fit.Draw('SAME')
+    v_0, phi_s = do_minuit(delta_phi_list[volts_min_index:], volts_list[volts_min_index:], delta_phi_err_list[volts_min_index:], [0.005 for i in volts_list[volts_min_index:]], volts_list[volts_min_index-2])
+    ndof = len(delta_phi_list[volts_min_index:])
+    chi2 = get_chi2(v_0, phi_s)
+    fit = get_chi2(v_0, phi_s)["graph"]
+    fit.Draw('p')
+    graph_plot.Draw('p')
+    xboa.common.root_wrapper.keep_root_object(fit)
     xboa.common.root_wrapper.keep_root_object(graph_fit)
     xboa.common.root_wrapper.keep_root_object(graph_plot)
-    xboa.common.root_wrapper.keep_root_object(fit)
-    
+    print "V_min", round(volts_list[volts_min_index], 5), "V_0", round(v_0, 5), "phi_s", round(phi_s, 5), "NDof", ndof,  "Chi2", round(chi2['score'], 5), "Prob", round(ROOT.TMath.Prob(chi2["score"], ndof), 5)
+    return (volts_list[volts_min_index], v_0, phi_s, ndof,  chi2['score'], ROOT.TMath.Prob(chi2["score"], ndof))
 
 def plot_phi_s(data):
-    canvas = xboa.common.make_root_canvas("phi_s")
-    hist, dummy = xboa.common.make_root_graph("", [0.], "Bunch phase [rad]", [0.], "RF Voltage [V]", xmin=2., xmax=6.0, ymin=0., ymax=4.)
-    hist.Draw()
 
     print "Fit 1"
-    delta_phi, delta_phi_err, volts = [], [], []
+    delta_phi, delta_phi_err, volts, fitted_fractions = [], [], [], []
     for i, a_datum in enumerate(data):
+        fitted_fractions.append(a_datum["fitted_fraction"])
         volts.append(a_datum["peak_to_peak_voltage"]/2.)
         rf_period = a_datum["rf_period"]
-        delta_phi.append(math.pi*2.*a_datum["fit_parameters"][3][0]/rf_period)
-        delta_phi_err.append(math.pi*2.*a_datum["fit_parameters"][3][1]/rf_period)
-#    do_fit(volts, delta_phi, delta_phi_err, 4, 1.1, 4.0)
-      
-    print "Fit 2"
-    delta_phi, delta_phi_err = [], []
-    for i, a_datum in enumerate(data):
-        rf_period = a_datum["rf_period"]
-        delta_phi.append(math.pi*2.*a_datum["mean_dt"]/rf_period)
-        delta_phi_err.append(math.pi*2.*a_datum["std_dt"]/rf_period)
-    do_fit(volts, delta_phi, delta_phi_err, 2, 0.4, 4.0)
-
-    delta_phi = []
-    for i, a_datum in enumerate(data):
-        rf_period = a_datum["rf_period"]
-        magnitude = -1.
-        #print i, volts[i], ":",
-        for dat in a_datum["peak_dt"]:
-            #print dat['magnitude'], math.pi*2.*dat['peak']/rf_period, "**",
-            if dat['magnitude'] > magnitude:
-                magnitude = dat['magnitude']
-                max_dphi = math.pi*2.*dat['peak']/rf_period
-        #print "Done", magnitude, max_dphi
-        delta_phi.append(max_dphi)
-    delta_phi[17] = 3.79013655584
-#    do_fit(volts, delta_phi, delta_phi_err, 8, 0.5, 2.5)
-
+        dt = (a_datum["peak_dt_max"]+a_datum["peak_dt_min"])/2.
+        if a_datum["peak_dt_max"] > a_datum["peak_dt_min"]:
+            dt_err = (a_datum["peak_dt_max"]-a_datum["peak_dt_min"])/2.
+        else:
+            dt_err = (a_datum["peak_dt_max"]+(632.-a_datum["peak_dt_min"]))/2.
+        delta_phi.append(math.pi*2.*dt/rf_period)
+        delta_phi_err.append(math.pi*2.*dt_err/rf_period)
+    make_fitted_fractions_graph(volts, fitted_fractions)
+    fit_values = []
+    canvas = xboa.common.make_root_canvas("phi_s")
+    hist, dummy = xboa.common.make_root_graph("", [0.], "Bunch phase [rad]", [0.], "RF Voltage [V]", xmin=0., xmax=math.pi*2., ymin=0., ymax=8.)
+    hist.Draw()
+    for v_min in volts[2:12]:
+        fit_values.append(do_fit(volts, delta_phi, delta_phi_err, 2, v_min-1e-6, 8.0))
+    print "Lowest voltage setting included [kV] & Fitted energy loss [keV] & Fitted phase [rad] & Number of degrees of freedom & $\\sum \\chi^2$ & Probability & Fraction of hits in cuts\\\\"
+    for fit in fit_values:
+        a_fitted_fraction = fitted_fractions[volts.index(fit[0])]
+        for value in fit[:-1]:
+            print round(value, 5), "&",
+        print round(fit[-1], 2), "&", a_fitted_fraction, "\\\\"
     canvas.Update()
     canvas.Print("plots/synchronous_phase_vs_voltage.root")
     canvas.Print("plots/synchronous_phase_vs_voltage.png")
 
+def make_fitted_fractions_graph(volts, fitted_fractions):
+    canvas = xboa.common.make_root_canvas("Fitted Fractions", "Fitted Fractions")
+    for x, y in zip(volts, fitted_fractions):
+        print x, y
+    hist, graph = xboa.common.make_root_graph("Fitted Fractions", fitted_fractions, "Fitted fraction", volts, "RF Voltage [kV]")
+    canvas.Draw()
+    hist.Draw()
+    graph.Draw()
+    canvas.Update()
+
+
 def main():
-    fin = open("data_summary.json")
+    fin = open("data_summary.ref")
     data = sorted([json.loads(line) for line in fin.readlines()], key = lambda x: x["peak_to_peak_voltage"])
     plot_phi_s(data)
     
